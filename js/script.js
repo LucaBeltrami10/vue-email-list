25c5ba46c1102cf085e4e13bc7be76bd084b2b80
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-12-12 14:51:32 +0100

    inserito metodo per utilizzo api in created

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            randomEmail : '',
 
         }
     },
@@ -12,6 +13,7 @@ createApp({
     },
 
     created(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => console.log(response) )
         
     }
 }) .mount('#app');
